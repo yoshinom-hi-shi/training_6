1b73b0aa90b1474393ba86c58915f4bcf2bdc712
yoshinom-hi-shi <nozomiyoshitaka@gmail.com>
2024-08-22 10:05:22 +0900

    pc_kv作成

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>training_6</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+   <div class="container"> 
+       <header class="header">
+            <img src="./img/stocksun_logo.png"  alt="Logo" class="logo">
+       </header>
+       <p class="sub-title">日本最大規模のフリーランスイベント</p>
+       <div class="kv"><img src="./img/kv.svg"></div>
+           <p class="schedule">
+                <span class="date">3/4</span>
+                <span class="day">sat</span>
+                <span class="date-time">10:00-18:00</span>
+           </p>
+           <div class="spot">東京国際フォーラム ホールE2</div>
+           <a href="registration-link" class="button">事前申し込みはこちら（無料）<img src="./img/LINE_Brand_icon 1.png" alt="LINE" class="line"></a>
+       </div>
+   </div>
+</body>
+</html>
