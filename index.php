2a8e200c5a5361c4a44e43cc3e881c3f9ae5d731
yoshinom-hi-shi <nozomiyoshitaka@gmail.com>
2024-08-26 16:31:38 +0900

    Event Summary まで作成

--- index.php
+++ index.php
@@ -8,22 +8,103 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-   <div class="container"> 
-       <header class="header">
-            <img src="./img/stocksun_logo.png"  alt="Logo" class="logo">
-       </header>
-       <p class="sub-title">日本最大規模のフリーランスイベント</p>
+    <div class="container" ><img src="./img/backdround.png" alt="backdroundr">
+       <div class="header"><img src="./img/stocksun_logo.png"  alt="Logo"></div>
+       <div class="sub-title">日本最大規模のフリーランスイベント</div>
        <div class="kv"><img src="./img/kv.svg"></div>
        <div class="kv_sp"><img src="./img/logo.png"></div>
        <div class="kv_sub_title"><img src="./img/sub_title.png"></div>
-           <p class="schedule">
-                <span class="date">3/4</span>
-                <span class="day">sat</span>
-                <span class="date-time">10:00-18:00</span>
-           </p>
-           <div class="spot">東京国際フォーラム ホールE2</div>
-           <a href="registration-link" class="button">事前申し込みはこちら（無料）<img src="./img/LINE_Brand_icon 1.png" alt="LINE" class="line"></a>
-       </div>
+       <div class="schedule">
+            <span class="date">3/4</span>
+            <span class="day">sat</span>
+            <span class="date-time">10:00-18:00</span>
+        </div>
+        <div class="spot">東京国際フォーラム ホールE2</div>
+        <div class="button"><img src="./img/button.png"  alt="LINELogo"></div>
    </div>
+
+   <section class=section id="about_section">
+        <div class="content">
+            <h1>Freelance Summit</h1>
+            <p>ーー  フリーランスサミットとは？  ーー</p>
+            </div>
+            <div class="explanation">「FREELANCE SUMMIT」は、StockSun株式会社が主催する 日本最大規模のフリーランスイベントです。</div >
+            <div class="explanation_sbu">各業界でTOPクラスのフリーランスが集結するイベント。有名企業が集結する40ブースもの出展エリアに加え、各領域で圧倒的に結果を出し続ける精鋭フリーランス達が、自身のノウハウを語ります。他のイベントでは味わえない、新たな体験と出会える充実したコンテンツを用意しています。</div >
+        </div>   
+   </section>   
+   <section class=section id="event_summary_section">
+        <div class="content">
+            <h1>Event Summary</h1>
+            <p>イベント開催概要</p>
+            <div class="event_item">
+                <div class="image_wrapper"><img src="./img/date.png" class="schedule_image"></div>    
+                <div class="text_wrapper"><p class="event_text">2023年3月4日（土）開場10:00 閉場18:00（受付開始9:30-）</p></div>
+            </div>   
+            <div class="event_item">
+                <div class="image_wrapper"><img src="./img/place.png" class="schedule_image"></div>    
+                <div class="text_wrapper"><p class="event_text">東京国際フォーラム ホールE2</p></div>
+            </div>  
+            <div class="event_item">
+                <div class="image_wrapper_second"><img src="./img/agenda.png" class="schedule_image"></div>    
+                <div class="text_wrapper">
+                    <div class="text_wrapper_second">
+                        <p class="event_text_main">企業ブース出展：60ブース</p>
+                        <p class="event_text_sub">ビジネス系セミナー：12枠</p>
+                    </div>
+                </div>
+            </div>  
+            <div class="event_item">
+                <div class="image_wrapper"><img src="./img/audience.png" class="schedule_image"></div>    
+                <div class="text_wrapper"><p class="event_text">経営者 / フリーランス / 独立志望の会社員</p></div>
+            </div>  
+            <div class="event_access_item">
+                <div class="image_wrapper_third"><img src="./img/access.png" class="schedule_image"></div>    
+                <div class="access_text_wrapper">
+                    <h2 class="access_text_main">【最寄駅】</h2>
+                    <li class="access_text_second">JR
+                        <h3>有楽町駅より徒歩1分</h3>
+                        <h3>東京駅より徒歩5分</h3>
+                    </li>
+                    <li class="access_text_third">地下鉄    
+                        <h3>有楽町線 ： 有楽町駅(B1F地下コンコースにて連絡）</h3>
+                        <h3>千代田線 ： 二重橋前駅より徒歩5分/日比谷駅より徒歩7分 </h3>
+                        <h3>丸ノ内線 ： 銀座駅より徒歩5分 </h3>
+                        <h3>銀座線 ： 銀座駅より徒歩7分/京橋駅より徒歩7分</h3>
+                        <h3>三田線 ： 日比谷駅より徒歩5分</h3>
+                    </li>
+               </div>
+               <div class="access_image_wrapper"><img src="./img/map.png" class="access_image_wrapper"></div>    
+           </div>  
+    </section>    
+    <section class=section id="content_section">
+        <div class="content">
+            <h1>Contents</h1>
+            <p>イベント内容</p>
+        </div>  
+    </section>   
+    <section class=section id="picup_section">
+        <div class="content">
+            <h1>Pick Up</h1>
+            <p>注目の企業</p>
+        </div>  
+    </section>   
+    <section class=section id="About Exhibitors_section">
+        <div class="content">
+            <h1>About Exhibitors</h1>
+            <p>出展企業一覧</p>
+        </div>  
+    </section>   
+    <section class=section id="timeschedule_section">
+        <div class="content">
+            <h1>Time Schedule</h1>
+            <p>タイムスケジュール</p>
+        </div>  
+    </section>  
+    <section class=section id="faq_section">
+        <div class="content">
+            <h1>FAQ</h1>
+            <p>よくある質問</p>
+        </div>  
+    </section>  
 </body>
 </html>
