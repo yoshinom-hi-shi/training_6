ce4caa25e91888d04613dd92890469bfec7b0986
yoshinom-hi-shi <nozomiyoshitaka@gmail.com>
2024-08-28 11:27:05 +0900

    PC_footerまで終了（ヘキサゴン未対応）

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <div class="container" ><img src="./img/backdround.png" alt="backdroundr">
+    <div class="container" >
        <div class="header"><img src="./img/stocksun_logo.png"  alt="Logo"></div>
        <div class="sub-title">日本最大規模のフリーランスイベント</div>
        <div class="kv"><img src="./img/kv.svg"></div>
@@ -26,36 +26,38 @@
    <section class=section id="about_section">
         <div class="content">
             <h1>Freelance Summit</h1>
-            <p>ーー  フリーランスサミットとは？  ーー</p>
+            <p class="subtitle">フリーランスサミットとは？</p>
             </div>
             <div class="explanation">「FREELANCE SUMMIT」は、StockSun株式会社が主催する 日本最大規模のフリーランスイベントです。</div >
             <div class="explanation_sbu">各業界でTOPクラスのフリーランスが集結するイベント。有名企業が集結する40ブースもの出展エリアに加え、各領域で圧倒的に結果を出し続ける精鋭フリーランス達が、自身のノウハウを語ります。他のイベントでは味わえない、新たな体験と出会える充実したコンテンツを用意しています。</div >
+            <div class="application-icon"><img src="./img/side_button.svg" class="schedule_image"></div>
         </div>   
    </section>   
+
    <section class=section id="event_summary_section">
         <div class="content">
             <h1>Event Summary</h1>
-            <p>イベント開催概要</p>
+            <p class="subtitle">イベント開催概要</p>
             <div class="event_item">
                 <div class="image_wrapper"><img src="./img/date.png" class="schedule_image"></div>    
-                <div class="text_wrapper"><p class="event_text">2023年3月4日（土）開場10:00 閉場18:00（受付開始9:30-）</p></div>
+                <div class="text_wrapper"><p id="event_text">2023年3月4日（土）開場10:00 閉場18:00（受付開始9:30-）</p></div>
             </div>   
             <div class="event_item">
                 <div class="image_wrapper"><img src="./img/place.png" class="schedule_image"></div>    
-                <div class="text_wrapper"><p class="event_text">東京国際フォーラム ホールE2</p></div>
+                <div class="text_wrapper"><p id="event_text">東京国際フォーラム ホールE2</p></div>
             </div>  
             <div class="event_item">
                 <div class="image_wrapper_second"><img src="./img/agenda.png" class="schedule_image"></div>    
                 <div class="text_wrapper">
                     <div class="text_wrapper_second">
-                        <p class="event_text_main">企業ブース出展：60ブース</p>
-                        <p class="event_text_sub">ビジネス系セミナー：12枠</p>
+                        <p id="event_text_main">企業ブース出展：60ブース</p>
+                        <p id="event_text_sub">ビジネス系セミナー：12枠</p>
                     </div>
                 </div>
             </div>  
             <div class="event_item">
                 <div class="image_wrapper"><img src="./img/audience.png" class="schedule_image"></div>    
-                <div class="text_wrapper"><p class="event_text">経営者 / フリーランス / 独立志望の会社員</p></div>
+                <div class="text_wrapper"><p id="event_text">経営者 / フリーランス / 独立志望の会社員</p></div>
             </div>  
             <div class="event_access_item">
                 <div class="image_wrapper_third"><img src="./img/access.png" class="schedule_image"></div>    
@@ -76,35 +78,280 @@
                <div class="access_image_wrapper"><img src="./img/map.png" class="access_image_wrapper"></div>    
            </div>  
     </section>    
+
     <section class=section id="content_section">
         <div class="content">
-            <h1>Contents</h1>
-            <p>イベント内容</p>
+            <h4>Contents</h4>
+            <p class="subtitle">イベント内容</p>
+            <div class=space_image>
+                <div class="space_image_left"><img src="./img/space_image_left.png" alt="space_image_left"></div>
+                <div class="space_image_right"><img src="./img/space_image_rigth.png" alt="space_image_right"></div>
+            </div>
+            <div class="button_second"><img src="./img/button.png"  alt="LINELogo"></div>
         </div>  
-    </section>   
+    </section>
+
     <section class=section id="picup_section">
         <div class="content">
-            <h1>Pick Up</h1>
-            <p>注目の企業</p>
+            <h5>Pick Up</h5>
+            <p class="subtitle">注目の企業</p>
+            <div class="picup_item">
+                <div class="oplan"><img src="./img/oplan.png" alt="oplan"></div>    
+                <div class="picup_wrapper"><p class="picup_text">オープラン株式会社は「ITサービス」を軸に「人」と「グローバル」に向き合い日本を代表する企業になるべく2015年に産声をあげたベンチャー企業です。</p>
+                    <a href="https://oplan.co.jp/" class="oplan_url">https://oplan.co.jp/</a>
+                </div>
+            </div>   
+            <div class="picup_item">
+                <div class="pe-bank"><img src="./img/PE-BANK.png" alt="pe-bank"></div>    
+                <div class="picup_wrapper"><p class="picup_text">ITフリーランスの仕事探しなら【PE-BANK】へ。Web・SE系を中心に毎月1,000件以上のご紹介。</p></div>
+            </div>   
+            <div class="picup_item">
+                <div class="boc-japan"><img src="./img/BOCjapan.png" alt="boc-japan"></div>    
+                <div class="picup_wrapper"><p class="picup_text">台湾の「食文化」の導入により、日本の飲食業をクリエイティブに。</p></div>
+            </div>   
+            <div class="picup_item">
+                <div class="creava"><img src="./img/creava.png" alt="creava"></div>    
+                <div class="picup_wrapper"><p class="picup_text">月額制システム開発チームレンタルSucSak(サクサク)を運営している東京新宿のシステム開発会社です。</p>
+                    <div class="sucsak_container">
+                        <img src="./img/sucsak.png" alt="sucsak">
+                        <a href="https://sucsak.com/" class="sucsak_url">：https://sucsak.com/</a>
+                    </div>
+                </div>
+            </div>   
+            <div class="picup_item">
+                <div class="quartet"><img src="./img/QUARTET.png" alt="quartet"></div>    
+                <div class="picup_wrapper"><p class="picup_text">1000業種以上の圧倒的運用実績！リスティング、Facebook・TwitterなどのSNS広告もおまかせ。</p></div>
+            </div>   
         </div>  
     </section>   
+
     <section class=section id="About Exhibitors_section">
         <div class="content">
             <h1>About Exhibitors</h1>
-            <p>出展企業一覧</p>
+            <p class="subtitle">出展企業一覧</p>
+            <div class=about_exhibitors>
+                <div class="about_exhibitors_left">
+                    <p>オープラン株式会社</p>
+                    <p>株式会社ビズリンク</p>
+                    <p>株式会社カルテットコミュニケーションズ</p>
+                    <p>クリーヴァ株式会社</p>
+                    <p>株式会社グラントホープ</p>
+                    <p>Modis株式会社</p>
+                    <p>INTLOOP株式会社</p>
+                    <p>Langdemy株式会社</p>
+                    <p>BOCジャパン合同会社</p>
+                </div>
+                <div class="about_exhibitors_right">
+                    <p>株式会社アユザック</p>
+                    <p>株式会社HandAid</p>
+                    <p>株式会社J-Proach</p>
+                    <p>ラーメン協会</p>
+                    <p>株式会社おウチdeお肉</p>
+                    <p>株式会社LifeCoach</p>
+                    <p>株式会社スポーツ＆ライフイノベーション</p>
+                    <p>株式会社iGO</p>
+                    <p>株式会社エドモンド</p>
+                </div>
+            </div>
+            <div class="attention">（順不同、一部企業のみ掲載）</div>
         </div>  
     </section>   
     <section class=section id="timeschedule_section">
         <div class="content">
-            <h1>Time Schedule</h1>
-            <p>タイムスケジュール</p>
+            <h6>Time Schedule</h6>
+            <p class="time_schedule">タイムスケジュール</p>
+            <div class="time_schedule_container">
+                <div class="time_schedule_item">
+                    <p id="hour">10:00〜</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <img src="./img/dj.png" alt="写真" class="logo item_phot">
+                    </div>   
+                    <div class="comment">
+                        <p id="comment">今、SNSを始めるなら</p>
+                    </div>  
+                </div>
+                <div class="time_schedule_item">
+                    <p id="hour">11:00〜</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <img src="./img/tabata.png" alt="写真" class="logo item_phot">
+                    </div>   
+                    <div class="comment">
+                        <p id="comment">会社員で出世する人と独立<br>して成功する人の適正につ<br>いて</p>
+                    </div>  
+                </div>
+                <div class="time_schedule_item">
+                    <p id="hour">11:40〜</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <img src="./img/sou.png" alt="写真" class="logo item_phot">
+                    </div>   
+                    <div class="comment">
+                        <p id="comment">営業出身のフリーランスの<br>生存戦略</p>
+                    </div>  
+                </div>
+                <div class="time_schedule_item">
+                    <p id="hour">12:20〜</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <img src="./img/hayashi.png" alt="写真" class="logo item_phot">
+                    </div>   
+                    <div class="comment">
+                        <p id="comment">無数のフリーランスに発注<br>してわかった優秀な人の特<br>徴</p>
+                    </div>  
+                </div>
+                <div class="time_schedule_item">
+                    <p id="hour">13:00〜</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <img src="./img/kinoshita.png" alt="写真" class="logo item_phot">
+                    </div>   
+                    <div class="comment">
+                        <p id="comment">現役上場企業社長が語る <br>最新WEBマーケティング論</p>
+                    </div>  
+                </div>
+                <div class="time_schedule_item">
+                    <p id="hour">13:40〜</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <img src="./img/kabumoto.png" alt="写真" class="logo item_phot">
+                    </div>   
+                    <div class="comment">
+                        <p id="comment">月収300万円までのロード<br>マップ</p>
+                    </div>  
+                </div>  
+                <div class="time_schedule_item">
+                    <p id="hour">14:20〜</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <div class="speaker">
+                            <img src="./img/yamamoto.png" alt="写真" class="logo item_phot_second_left">
+                            <p id="fullname">やまもと <br>りゅうけん</p>
+                        </div>
+                        <div class="speaker">
+                            <img src="./img/sako.png" alt="写真" class="logo item_phot_second_center">
+                            <p id="fullname_center">迫佑樹</p>
+                        </div>
+                        <div class="speaker">
+                            <img src="./img/aosasa.png" alt="写真" class="logo item_phot_second_right">
+                            <p id="fullname_right">青笹寛史</p>
+                        </div>
+                        <img src="./img/yama_sako_ao.png" alt="写真" class="logo item_phot_second_name">
+                        <img src="./img/x.png" alt="写真" class="logo item_phot_x_left"> 
+                        <img src="./img/x.png" alt="写真" class="logo item_phot_x_right"> 
+                    </div>   
+                    <div class="comment">
+                        <p id="comment">SNSを伸ばした先に見える<br>世界</p>
+                    </div>  
+                </div>
+                <div class="time_schedule_item">
+                    <p id="hour">15:00〜</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <img src="./img/inoue.png" alt="写真" class="logo item_phot">
+                    </div>   
+                    <div class="comment">
+                        <p id="comment">元サイバーエージェント局<br>長による広告業界の未来予<br>想図</p>
+                    </div>  
+                </div>  
+                <div class="time_schedule_item">
+                    <p id="hour">15:40〜</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <img src="./img/itabashi.png" alt="写真" class="logo item_phot_third_left">
+                        <img src="./img/aikuchi.png" alt="写真" class="logo item_phot_third_right">
+                        <img src="./img/ita_ai.png" alt="写真" class="logo item_phot_name">
+                        <img src="./img/x.png" alt="写真" class="logo item_phot_x">
+                    </div>   
+                    <div class="comment">
+                        <p id="comment">エンジニアとして独立し、<br>年収3000万円を超える方法</p>
+                    </div>  
+                </div>  
+                <div class="time_schedule_item">
+                    <p id="hour">16:20〜</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <img src="./img/iwai.png" alt="写真" class="logo item_phot">
+                    </div>   
+                    <div class="comment">
+                        <p id="comment">累計300人以上、起業家を<br>見て感じた成功法則</p>
+                    </div>  
+                </div>  
+                <div class="time_schedule_item">
+                    <p id="hour">17:00〜</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <img src="./img/shimabukuro.png" alt="写真" class="logo item_phot">
+                    </div>   
+                    <div class="comment">
+                        <p id="comment">フリーランスにおすすめの<br>M＆Aトレンドを紹介</p>
+                    </div>  
+                </div>               
+                <div class="time_schedule_item_sub">
+                    <p id="hour">MC</p>
+                    <div class="logo_hour">
+                        <img src="./img/time.svg" alt="時間" class="logo item_time">
+                        <img src="./img/uemoto_kane.png" alt="写真" class="logo item_phot_force">
+                    </div>   
+                </div>
+            </div>
         </div>  
     </section>  
+
     <section class=section id="faq_section">
         <div class="content">
-            <h1>FAQ</h1>
-            <p>よくある質問</p>
-        </div>  
+            <h5>FAQ</h5>
+            <p class="subtitle">よくある質問</p>
+            <div class="faq_container">
+                <div class="qa">
+                    <div class="question">
+                        <div class="circle_blue">
+                            <span>Q</span>
+                        </div>
+                        <ul class="qa_text">再入場はできますか？</ul>
+                    </div>
+                    <div class="answer">
+                        <div class="circle_yellow">
+                            <span>A</span>
+                        </div>
+                        <ul class="qa_text">できます。</ul>
+                    </div>  
+                </div>
+                <div class="qa">
+                    <div class="question">
+                        <div class="circle_blue">
+                            <span>Q</span>
+                        </div>
+                        <ul class="qa_text">入場にお金はかかりますか？</ul>
+                    </div>
+                    <div class="answer">
+                        <div class="circle_yellow">
+                            <span>A</span>
+                        </div>
+                        <ul class="qa_text">一切かかりません。</ul>
+                    </div>  
+                </div>
+                <div class="qa">
+                   <div class="question">
+                        <div class="circle_blue">
+                            <span>Q</span>
+                        </div>
+                        <ul class="qa_text">出展を希望する企業はどこから問い合わせれば良いですか？</ul>
+                    </div>
+                    <div class="answer">
+                        <div class="circle_yellow">
+                            <span>A</span>
+                        </div>
+                        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdphU7Vu9xdSpRV0xaVSt9C6MpHUYG3OsTh7Vuqj8sGyInYqA/viewform" id="qa_text_link">こちらからお願いします</a>
+                    </div>  
+                </div>
+            </div>
     </section>  
+    <footer>
+        <div class="footer_container">
+            <div class="footer_border"></div><img src="/img/footer_border.png" alt="footer_border">
+        </div>
+    </footer>
 </body>
 </html>
